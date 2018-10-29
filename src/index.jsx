38ae4f070b6454c006406544ea804c1a93a6c12d
Louis Riehl <louisriehl@gmail.com>
2018-10-29 19:00:10 +0000

    message container and messages are components

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
-ReactDOM.render(<ChatBar />, document.getElementById('react-root'));
+const AppContents = (
+  <div>
+    {/*<App/ >  Currently not doing anything */}
+    <MessageList />
+    <ChatBar />
+  </div>);
+
+ReactDOM.render(AppContents, document.getElementById('react-root'));
