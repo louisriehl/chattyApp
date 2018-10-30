e5235769a5ec1b386cc3fdfd4d9772b7cdbb4c7b
Louis Riehl <louisriehl@gmail.com>
2018-10-30 17:18:42 +0000

    can connect to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -36,18 +36,10 @@ class App extends Component {
     this.setState({messages: messages, idTracker: this.state.idTracker + 1});
   }
 
-  // componentDidMount() {
-  //   console.log("componentDidMount <App />");
-  //   setTimeout(() => {
-  //     console.log("Simulating incoming message");
-  //     // Add a new message to the list of messages in the data store
-  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!", type:"incomingMessage"};
-  //     const messages = this.state.messages.concat(newMessage)
-  //     // Update the state of the app component.
-  //     // Calling setState will trigger a call to render() in App and all child components.
-  //     this.setState({messages: messages})
-  //   }, 3000);
-  // }
+  componentDidMount() {
+    const self = this;
+    this.socket = new WebSocket("ws://0.0.0.0:3001");
+  }
 
   render() {
     return (
