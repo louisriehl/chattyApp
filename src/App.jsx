21c23152256afef0caa229e4c8b1663d7387c6d3
Louis Riehl <louisriehl@gmail.com>
2018-10-30 14:23:57 +0000

    passing username to chatbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="app">
       <MessageList />
-      <ChatBar />
+      <ChatBar currentUser={this.state.currentUser}/>
       </div>
     );
   }
