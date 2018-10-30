a72f23eb6097bbcf1fd123363036899a31bf2f44
Louis Riehl <louisriehl@gmail.com>
2018-10-30 14:17:57 +0000

    restructured component hierarchy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1></h1>
+      <div className="app">
+      <MessageList />
+      <ChatBar />
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,5 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import ChatBar from './ChatBar.jsx';
-import MessageList from './MessageList.jsx';
 
-const AppContents = (
-  <div>
-    {/*<App/ >  Currently not doing anything */}
-    <MessageList />
-    <ChatBar />
-  </div>);
-
-ReactDOM.render(AppContents, document.getElementById('react-root'));
+ReactDOM.render(<App />, document.getElementById('react-root'));
