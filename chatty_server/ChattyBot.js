ef55ba997d943fc02dacd87801bac0cdfec3390f
Louis Riehl <louisriehl@gmail.com>
2018-11-01 14:59:51 +0000

    added new chattybot commands

--- chatty_server/ChattyBot.js
+++ chatty_server/ChattyBot.js
@@ -5,17 +5,22 @@ const chattyBot = {
     const command = chattyBot.parseString(string);
     let response = "";
     switch(command) {
+      case "howdy":
+      case "hi":
       case "hello":
         response = "Hello there human!";
         break;
+      case "help":
+        response = "No worries, human, chattybot is here! Try one of these commands:" +
+        "\n chattybot hello";
+        break;
       default:
-        response = "I don't recognize that command!";
+        response = "I don't recognize that command!" +
+        "Try typing 'chattybot help' to see what I can do!";
     }
     return response;
   },
   parseString: (command) => command.replace(chattyBot.regex, "")
 };
 
-console.log(chattyBot.regex);
-console.log(chattyBot.isCalling('chattybotsays hi!'));
-console.log(chattyBot.giveAnswer("chattybot hello"));
+console.log(chattyBot.giveAnswer("chattybot help"));
